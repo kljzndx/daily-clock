e1051bdca1954040d29ae0654c36673edc30d346
李继龙 <kljzndx@outlook.com>
2024-03-24 08:46:33 +0800

    添加数据表类型“TimeRecord”

---
+++ DailyClock/Services/Tables/TimeRecord.cs
@@ -0,0 +1,34 @@
+﻿using FreeSql.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyClock.Services.Tables
+{
+    [Table]
+    public record TimeRecord
+    {
+        public TimeRecord()
+        {
+            
+        }
+
+        public TimeRecord(string title, string message, DateTime beginTime, DateTime endTime)
+        {
+            Title = title;
+            Message = message;
+            BeginTime = beginTime;
+            EndTime = endTime;
+        }
+
+        [Column(IsPrimary = true, IsIdentity = true)]
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public DateTime BeginTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
