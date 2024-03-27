13af4e1a92709d38e35da10e7a16bc5bbd907efd
李继龙 <kljzndx@outlook.com>
2024-03-27 13:40:17 +0800

    新建RecordTag表

---
+++ DailyClock/Services/Tables/Mapping_TimeRecord_Category.cs
@@ -0,0 +1,23 @@
+﻿using FreeSql.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyClock.Services.Tables
+{
+    [Table]
+    public record Mapping_TimeRecord_Tag
+    {
+        [Column(IsPrimary = true, IsIdentity = true)]
+        public long Id { get; set; }
+
+        public long RecordId { get; set; }
+        public TimeRecord? Record { get; set; }
+
+        public long TagId { get; set; }
+        public RecordTag? Tag { get; set; }
+    }
+}
---
+++ DailyClock/Services/Tables/RecordTag.cs
@@ -0,0 +1,32 @@
+﻿using FreeSql.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyClock.Services.Tables
+{
+    [Table]
+    public record RecordTag
+    {
+        [Column(IsPrimary = true, IsIdentity = true)]
+        public long Id { get; set; }
+        public string Name { get; set; } = "";
+        [Column(StringLength = -1)]
+        public string Comment { get; set; } = "";
+
+        public string Icon { get; set; } = "";
+        public string IconType { get; set; } = "Text";
+        public string Color { get; set; } = "";
+
+        public long ParentId { get; set; }
+        public RecordTag? Parent { get; set; }
+
+        [Column(ServerTime = DateTimeKind.Utc, CanUpdate = false)]
+        public DateTime CreateedTime { get; set; }
+        [Column(ServerTime = DateTimeKind.Utc)]
+        public DateTime EditedTime { get; set; }
+    }
+}
