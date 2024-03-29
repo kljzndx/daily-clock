3e9271488e55040bf82ae0b7f4db8887d43b255b
李继龙 <kljzndx@outlook.com>
2024-03-29 13:17:52 +0800

    清空测试元素

--- DailyClock/ViewModels/MainViewModel.cs
+++ DailyClock/ViewModels/MainViewModel.cs
@@ -15,51 +15,8 @@ using System.Threading.Tasks;
 
 namespace DailyClock.ViewModels
 {
-    public partial class MainViewModel(ILogger<MainViewModel> logger, AppSettings conf, IFreeSql fsql) : ObservableRecipient
+    public partial class MainViewModel(ILogger<MainViewModel> logger, IFreeSql fsql) : ObservableRecipient
     {
-        [ObservableProperty]
-        private string _mes = "Nomal";
-        [ObservableProperty]
-        private string _timeMes = "";
-
-        [ObservableProperty]
-        private ObservableCollection<TimeRecord> _data = new();
-
-        [RelayCommand]
-        private void Hello()
-        {
-            logger.LogInformation("Hello World");
-            Mes = "Logged";
-        }
-
-        [RelayCommand]
-        private void TestConf()
-        {
-            logger.LogInformation("Testing read conf");
-            Mes = conf.Test1;
-        }
-
-        [RelayCommand]
-        private void TestReadDb()
-        {
-            Data.Clear();
-            foreach (var item in fsql.Select<TimeRecord>().ToList(a => a))
-            {
-                Data.Add(item);
-            }
-        }
-
-        [RelayCommand]
-        private async Task TestWriteDb()
-        {
-            DateTime now = DateTime.Now;
-            DateTime before = now - TimeSpan.FromMinutes(5);
-
-            TimeRecord record = new(TimeMes, TimeMes, before, now);
-
-            record.Id = await fsql.Insert<TimeRecord>().AppendData(record).ExecuteIdentityAsync();
-            Data.Add(record);
-
-        }
+        
     }
 }
