f86cb9e149bf0cc944b5f31adb81dce6e280e0be
李继龙 <kljzndx@outlook.com>
2024-05-04 08:22:50 +0800

    给记录器增加时间显示

---
+++ DailyClock/ViewModels/ValueConverters/DateTimeToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DailyClock.ViewModels.ValueConverters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not DateTime dt) return "This is not DateTime";
+
+            return dt.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string sdt) return default(DateTime);
+
+            return DateTime.Parse(sdt).ToUniversalTime();
+        }
+    }
+}
